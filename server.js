61cabd257bfe75fad520131333c9012869bde1c2
Ira Noschenko <i.noschenko@gmail.com>
2018-04-07 12:23:39 +0300

    api -> started

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-const express = require('express');
-const path = require('path');
-const serveStatic = require('serve-static');
+const express = require('express'),
+	app = express(),
+	mongoose = require('mongoose'),
+	User = require('./api/models/userListModel'),
+	bodyParser = require('body-parser');
 
-let app = express();
-app.use(serveStatic(__dirname + "/dist"));
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Userdb');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const routes = require('./api/routes/userListRoutes');
+routes(app);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
